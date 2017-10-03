df47a2e826af78e9a16d66be33e6b5d3380eb155
michal.makowej <michal.makowej@MMAKOWEJ>
2017-10-03 08:41:51 +0200

    Initial

---
+++ Clock/Clock.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Clock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ClockTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.ClockTimer.Interval = 1000;
+            this.ClockTimer.Tick += new System.EventHandler(this.ClockTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 851);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Timer ClockTimer;
+    }
+}
+
---
+++ Clock/Clock.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Clock : Form
+    {
+        Label SecondsLabel = new Label();
+        Label MinutesLabel = new Label();
+        Label HoursLabel = new Label();
+
+        public Clock()
+        {
+            InitializeComponent();
+            SetHours();
+            SetMinutes();
+            SetSeconds();
+            
+            Text = DateTime.Now.ToString();
+
+            ClockTimer.Start();
+        }
+
+        private void SetHours()
+        {
+            int x = 5;
+
+            for (int i = 0; i <= 23; i++)
+            {
+                Label label = new Label();
+                label.AutoSize = true;
+                label.Location = new Point(5, x);
+                label.Font = new Font("Arial", 10);
+                label.Text = i.ToString();
+
+                Controls.Add(label);
+
+                x += 36;
+            }
+
+            HoursLabel.AutoSize = true;
+            HoursLabel.Location = new Point(35, DateTime.Now.Hour * 36 + 5);
+            HoursLabel.Text = "------------------------------";
+
+            Controls.Add(HoursLabel);
+        }
+
+        private void SetMinutes()
+        {
+            int x = 8;
+
+            for (int i = 0; i <= 59; i++)
+            {
+                Label label = new Label();
+                label.AutoSize = true;
+                label.Location = new Point(140, x);
+                label.Font = new Font("Arial", 8);
+                label.Text = i.ToString();
+
+                Controls.Add(label);
+
+                x += 14;
+            }
+
+            MinutesLabel.AutoSize = true;
+            MinutesLabel.Location = new Point(160, DateTime.Now.Minute * 14 + 8);
+            MinutesLabel.Text = "--------------------------";
+
+            Controls.Add(MinutesLabel);
+        }
+
+        private void SetSeconds()
+        {
+            int x = 8;
+
+            for (int i = 0; i <= 59; i++)
+            {
+                Label label = new Label();
+                label.AutoSize = true;
+                label.Location = new Point(250, x);
+                label.Font = new Font("Arial", 8);
+                label.Text = i.ToString();
+
+                Controls.Add(label);
+
+                x += 14;
+            }
+
+            SecondsLabel.AutoSize = true;
+            SecondsLabel.Location = new Point(270, DateTime.Now.Second * 14 + 8);
+            SecondsLabel.Text = "------------------------------";
+
+            Controls.Add(SecondsLabel);
+        }
+
+        
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            Text = DateTime.Now.ToString();
+            
+            SecondsLabel.Location = new Point(270, DateTime.Now.Second * 14 + 8);
+            MinutesLabel.Location = new Point(160, DateTime.Now.Minute * 14 + 8);
+            HoursLabel.Location = new Point(35, DateTime.Now.Hour * 36 + 5);
+        }
+    }
+}
